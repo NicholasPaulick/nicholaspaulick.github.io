a99d0a0e9d024bf7f0404b66be393ba8385c399b
Nicholas Paulick <76536219+NicholasPaulick@users.noreply.github.com>
2021-08-27 09:53:33 -0500

    Update javascripts.js

--- javascripts.js
+++ javascripts.js
@@ -1,5 +1,7 @@
+//import * as PIXI from 'pixi.js'
+
 function funct() {
-    var Var = "Im working on this button now.";
+    let Var = "Im working on this button now.";
     alert(Var);
     //let app = new PIXI.Application({ width: 640, height: 360 });
 }
